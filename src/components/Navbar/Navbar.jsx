5d6a3143b4c46e7b89c7b86d8724707acf0a799e
Durli <hojayevadurli@gmail.com>
2023-06-28 17:58:12 -0600

    added header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
 import './Navbar.css';
-
+import images from "../../constants/images"
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className='app__navbar'>
+    <div className='app__navbar-logo'>
+      <img src={images.gericht} alt="app logo"></img>
+      <ul className='app__navbar-links'>
+        <li className='p__opensans'><a href='#home'>Home</a> </li>
+          <li className='p__opensans'><a href='#about'>About</a></li>
+          <li className='p__opensans'><a href='#menu'>Menu</a></li>
+          <li className='p__opensans'><a href='#awards'>Awards</a></li>
+          <li className='p__opensans'><a href='#contact'>Contact</a></li>
+       
+      </ul>
+    </div>
+    <div className='app__navbar-login'>
+      <a href='/' className='p__opensans'>Book Table</a>
+      <a href='#login' className='p__opensans'>Log In/Register</a>      
+    </div>
+    
+  </nav>
 );
 
 export default Navbar;
+ 
