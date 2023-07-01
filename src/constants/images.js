3390a0ecebdbf2800f31ef031eb4767116c8cbfa
Durli <hojayevadurli@gmail.com>
2023-06-30 20:03:30 -0600

    changed the header image and added the overlay

--- src/constants/images.js
+++ src/constants/images.js
@@ -20,6 +20,8 @@ import award05 from '../assets/award05.png';
 import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
+import r from '../assets/r.png';
+import res from '../assets/res.png';
 
 export default {
   bg,
@@ -44,4 +46,6 @@ export default {
   sign,
   quote,
   gericht,
+  r,
+  res,
 };
